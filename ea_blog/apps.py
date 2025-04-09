10899670f2b0b7a93656297cb5caaf0f8672aa02
Ellen Alves <milesyralves@gmail.com>
2025-04-09 14:21:14 -0300

    primeiro commit

---
+++ ea_blog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EaBlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blog'
---
+++ ea_blog/settings.py
@@ -0,0 +1,97 @@
+import os
+from pathlib import Path
+
+# Caminho base do projeto
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Segurança
+SECRET_KEY = 'null'
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+ROOT_URLCONF = 'blog.urls'
+# Configuração do servidor de email
+
+# Aplicações instaladas
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'blog',
+]
+
+
+# Middleware
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Configuração do banco de dados
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Configurações de templates
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            BASE_DIR / "templates/",
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Configuração do WSGI
+WSGI_APPLICATION = 'ea_blog.wsgi.application'
+
+# Arquivos estáticos
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+# Configuração do idioma e fuso horário
+LANGUAGE_CODE = 'pt-br'
+TIME_ZONE = 'America/Sao_Paulo'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Configuração de usuários
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Configuração do sistema de arquivos estáticos
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
